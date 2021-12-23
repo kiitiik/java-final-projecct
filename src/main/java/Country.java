b47e3b2be7c38b32d22bc87f9e6189bf36c16ef0
kiitiik <v1taliy-yak@yandex.ru>
2021-12-23 23:48:16 +0500

    Added class to describe the Country entity

---
+++ src/main/java/Country.java
@@ -0,0 +1,15 @@
+public class Country {
+    public String Name;
+    public String Region;
+    public int HappinessRank;
+    public double HappinessScore;
+    public double LowerConfidenceInterval;
+    public double UpperConfidenceInterval;
+    public double Economy;
+    public double Family;
+    public double Health;
+    public double Freedom;
+    public double Trust;
+    public double Generosity;
+    public double DystopiaResidual;
+}
